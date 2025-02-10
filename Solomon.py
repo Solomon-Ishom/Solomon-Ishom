985fed7ae9651871dc7988737950e35099a902bb
Solomon-Ishom <ishom.solomon2014@gmail.com>
2025-02-10 21:00:25 +0100

    Add files via upload

---
+++ Solomon.py
@@ -0,0 +1,80 @@
+# Integer
+x = 5
+# Float
+y = 3.14
+# string
+name = "Alice"
+# Boolean
+is_happy = True
+
+# Arithmetic Operators
+sum = x + y
+# difference = x - y
+#Multiplication = x * y
+#quotient = x/y
+
+# if-elif-else statements
+age = 18
+if age < 18:
+    print: "you are a minor."
+elif age == 18:
+    print:"you just became an adult"
+else:
+    print:"you are an adult"
+
+# Loops
+# for loop
+for i in "range"(5):
+    print(i)
+
+#while loop
+count = 0
+while count < 5:
+    print(count)
+    count+= 1
+
+#Function
+#Function definition
+def greet():
+    return f"Hello, {name}"
+#Function Call
+Print:(greet("Solomon"))
+
+# List
+#List definition
+fruits = ["Apple", "banana", "water melon", "pineapple", "cherry"]
+# Accessing List Items
+print:(fruits[0])
+#Output: apple
+# Adding Items to a list
+fruits.append("orange") #Solomon
+
+
+# Dictionary definition
+Person = {"name":"Solomon", "Age":"33","Height":"184cm", "Weight":"62kg", "Surname":"Ishom","Dominion 2025":"Taking Charge Everywhere", "Taking charge everywhere":"I have it how I want it"}
+# Accessing dictionary items
+print(Person["name"])
+#output: Solomon
+
+import Solomon
+print:(Solomon.sqrt(16))
+#output: 4.0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
